67a59af0b0e7508f4c65f43c85e8204082ae2c90
Julia Kazanskaya <kazanskaya.julia@gmail.com>
2024-06-25 16:34:21 +0300

    добавили кнопочки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Button from "@/UI/Button"
+import Input from "@/UI/Input"
 
 export default function Home() {
   return (
     <main className="flex">
-      <h1>hello world</h1>
+      <h1> Hello brazze! </h1>
+        <div className="main-block">
+          <Input></Input>
+          <div></div>
+          <Button text={"test text"} color={"red"}></Button>
+          <Button text={"test 222s text"} color={"green"}></Button>
+        </div>
     </main>
   );
 }
